782c10e90f9a73de10219ac2e756c0c9b07fa298
Harsh <harsh00var@gmail.com>
2023-11-14 12:57:01 +0530

    initial commit

---
+++ app/src/main/java/com/example/firestorerecycler/User.java
@@ -0,0 +1,41 @@
+package com.example.firestorerecycler;
+
+public class User
+{
+
+    String firstName,lastName;
+    String age;
+
+    public User()
+    {}
+
+    public User(String firstName, String lastName, String age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
